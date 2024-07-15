d4937192a042d0c9aa652acc5db834fc0e7ca91b
Эмиль Холоднов <e.kholodnov@just-work.org>
2024-07-15 22:37:05 +0500

    Beta v2

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ install:
 	echo "Copy text from '.gitattibutes' to super '.gitattibutes'"
 	echo "Add text from 'gitconfig' file to super '.git/config'"
 	echo "Add '.gitencrypt_secrets' to '.gitignore'"
-
-PHONY: install
+	git config core.fileMode false
+	chmod +x gitconfig_*
+	chmod +x base_script
--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
 import os
 import subprocess
+import unittest
 
-def eval_command(*commands, return_binary=False, print_command=False):
+DEBUG=False
+
+
+def eval_command(*commands, return_binary=False):
     process = subprocess.Popen(
         # For UNIX
         # ['/bin/bash'], 
@@ -19,92 +23,86 @@ def eval_command(*commands, return_binary=False, print_command=False):
     )
     command_list = [
         ". ./base_script", 
+        "function get_creds() { echo 'some_strong_password' 'ca475acf0f1ca4b0'; }"
         # "set -evxo pipefail -B"
     ] + list(commands)
     commands = " && ".join(command_list)
-    if print_command:
+    if DEBUG:
         print(f"{commands!r}")
 
     output, exit_code = process.communicate(input=commands.encode())
     assert exit_code == None
-    result = output if return_binary else output.decode(errors='ignore') 
-    print(f"{result!r}")
+    result = output if return_binary else output.decode(errors='ignore')
+    if DEBUG:
+        print(f"{result!r}")
     return result
 
 def pipeline(*commands):
     return " | ".join(commands)
 
-def run_tests():
-    base64_encode = "base64"
-    base64_decode = f"{base64_encode} -d"
-    binary_to_hex = "binary_to_hex"
-    hex_to_binary = "hex_to_binary"
-    remove_null_byte = "tr -d \"\\r\\n\\0\""
-    add_hex_prefix = "sed \"s/^/{53616c7465645f5f${GITENCRYPT_SALT_FIXED}}/\""
 
+class TestStringMethods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # base64_encode = "base64"
+        # base64_decode = f"{base64_encode} -d"
+        # binary_to_hex = "binary_to_hex"
+        # hex_to_binary = "hex_to_binary"
+        # remove_null_byte = "tr -d \"\\r\\n\\0\""
+        cls.encoded_value = "secrets"
+        cls.encoded_value_with_openssl_version_1_1_1 = "U2FsdGVkX1/KR1rPDxyksAB2r3M/7XTT8xt3bJLksH4=\n"
+        cls.encoded_value_with_openssl_version_3_0_13 = "AHavcz/tdNPzG3dskuSwfg==\n"
+        
+    def test_encode(self):
+        encoded_value = eval_command(
+            pipeline(
+                "echo -n 'secrets'",
+                "gitencrypt_crypt",
+            ),
+        )
+        self.assertEqual(encoded_value, "U2FsdGVkX1/KR1rPDxyksAB2r3M/7XTT8xt3bJLksH4=\n")
 
-    # TODO: Нужно в binary записать соль, а потом уже конвертить в base64
-    encoded_value = "secrets"
-    encoded_value_with_openssl_version_1_1_1 = "U2FsdGVkX1/KR1rPDxyksAB2r3M/7XTT8xt3bJLksH4=\n"
-    encoded_value_with_openssl_version_3_0_13 = "AHavcz/tdNPzG3dskuSwfg==\n"
+        decoded_value = eval_command(
+            pipeline(
+                f"echo -en '{self.encoded_value_with_openssl_version_1_1_1}'",
+                "gitencrypt_decrypt",
+            ),
+        )
+        self.assertEqual(decoded_value, "secrets")
 
-    # ---
-    # TODO: Check this for both versions
-    encoded_value = eval_command(
-        pipeline(
-            "echo -n 'secrets'",
-            "gitencrypt_crypt",
-        ),
-    )
-    assert encoded_value == "U2FsdGVkX1/KR1rPDxyksAB2r3M/7XTT8xt3bJLksH4=\n"
+        decoded_value = eval_command(
+            pipeline(
+                f"echo -en '{self.encoded_value_with_openssl_version_3_0_13}'",
+                "gitencrypt_decrypt",
+            ),
+        )
+        self.assertEqual(decoded_value, "secrets")
 
-    decoded_value = eval_command(
-        pipeline(
-            f"echo -en '{encoded_value_with_openssl_version_1_1_1}'",
-            "gitencrypt_decrypt",
-        ),
-    )
-    assert decoded_value == "secrets"
+        decoded_value = eval_command(
+            pipeline(
+                """
+                a="SGVsbG8K"
+                is_text_base64 $a
+                echo "$?"
+                """
+            ),
+        )
 
-    decoded_value = eval_command(
-        pipeline(
-            f"echo -en '{encoded_value_with_openssl_version_3_0_13}'",
-            "gitencrypt_decrypt",
-        ),
-    )
-    assert decoded_value == "secrets"
+        self.assertEqual(decoded_value, "0\n")
 
-    decoded_value = eval_command(
-        pipeline(
-            """
-            a="SGVsbG8K"
-            if is_text_base64 $a; then
-                echo -en "base64"
-            else
-                echo -en "NOT base64"
-            fi
-            """
-        ),
-    )
+        decoded_value = eval_command(
+            pipeline(
+                """
+                a="Hello"
+                is_text_base64 $a
+                echo "$?"
+                """
+            ),
+        )
 
-    assert decoded_value == "base64"
+        self.assertEqual(decoded_value, "1\n")
 
-    decoded_value = eval_command(
-        pipeline(
-            """
-            a="Hello"
-            if is_text_base64 $a; then
-                echo -en "base64"
-            else
-                echo -en "NOT base64"
-            fi
-            """
-        ),
-    )
-
-    assert decoded_value == "NOT base64"
-
-    # ---
+        # ---
 
 if __name__ == "__main__":
-    run_tests()
+    unittest.main()
